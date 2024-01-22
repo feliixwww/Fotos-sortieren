e508efe640fd57558f1d83f361e8dc1ffc26b520
feliixwww <felix@soul-werner.de>
2024-01-22 14:23:36 +0100

    Update datumentnahme.py

--- datumentnahme.py
+++ datumentnahme.py
@@ -1,10 +1,2 @@
 
 
-
-
-def kopieren(source, destination):
-    shutil.copy(source, destination)
-
-
-
-kopieren(rf'C:\Users\intern1\Pictures\sortierte-bilder', rf'C:\Users\intern1\Pictures\sortierte-bilder\{datum[0]}')
